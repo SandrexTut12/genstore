3f34946f347f45ea21189139ce4f42431c5deb9a
tutar <tutarashvilis@gmail.com>
2026-06-18 14:21:18 +0400

    Modal Facebook button now opens Messenger direct chat via m.me link

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 // ============ CONFIG ============
 const CONFIG = {
   facebook  : "https://www.facebook.com/profile.php?id=61590242855257",
+  messenger : "https://m.me/1093496020521668",
   instagram : "https://www.instagram.com/genstore856/",
   whatsapp  : "500700362",
   phone     : "",
@@ -290,7 +291,7 @@ function openProduct(id) {
       ).join("")}</div>`
     : "";
 
-  const ctaFb = `<a class="btn btn-cta btn-cta-fb" href="${fb}" target="_blank" rel="noopener">
+  const ctaFb = `<a class="btn btn-cta btn-cta-fb" href="${CONFIG.messenger || fb}" target="_blank" rel="noopener">
     <svg width="17" height="17" viewBox="0 0 24 24" fill="currentColor"><path d="M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.05V9.41c0-3.02 1.79-4.69 4.53-4.69 1.31 0 2.69.24 2.69.24v2.97h-1.52c-1.49 0-1.96.93-1.96 1.89v2.25h3.33l-.53 3.49h-2.8V24C19.61 23.1 24 18.1 24 12.07z"/></svg>
     Facebook
   </a>`;
